3999e32b98cc75220a840119fdbf08e12ad177e3
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-03 14:16:17 -0400

    upload issues

--- tools/scrape/upload/upload_db.py
+++ tools/scrape/upload/upload_db.py
@@ -32,6 +32,15 @@ def titles():
     return titles
 
 
+def issues():
+    issues = []
+    with open("data/clean/issues.txt") as issues_file:
+        for line in issues_file:
+            issues.append(eval(line.strip()))
+
+    return issues
+
+
 def insert_publishers():
     with db_conn() as conn:
         cur = conn.cursor()
@@ -116,8 +125,35 @@ def insert_titles():
             cur.copy_from(titles_csv, "titles", columns=("id", "name", "publisher_id"))
 
 
+def insert_issues():
+    with open("data/upload-cache/titles_dict.txt") as titles_file:
+        titles = eval(titles_file.read())
+
+    titles_rev = {}
+    for uid, p in titles.items():
+        titles_rev[p["id"]] = uid
+
+    ns = issues()
+    issues_dict = {str(uuid.uuid4()): i for i in ns}
+    print("writing cache")
+    with open("data/upload-cache/issues_dict.txt", "w") as issues_file:
+        issues_file.write(str(issues_dict))
+
+    print("writing csv")
+    with open("/tmp/issues.csv", "w") as issues_csv:
+        for uid, i in issues_dict.items():
+            tuid = titles_rev[i["title_id"]]
+            issues_csv.write(f"{uid}\t{i['number'].strip()}\t{tuid}\n")
+
+    print("writing to db")
+    with db_conn() as conn:
+        cur = conn.cursor()
+        with open("/tmp/issues.csv") as issues_csv:
+            cur.copy_from(issues_csv, "issues", columns=("id", "name", "title_id"))
+
+
 def main():
-    insert_titles()
+    insert_issues()
 
 
 if __name__ == "__main__":
