0b583f7c0c19900f353e3e1fdfdfec8f192d0403
randomicons <randomicons@users.noreply.github.com>
2022-05-03 19:35:03 -0500

    Add issue details endpoint (#16)
    * Add issue details endpoint
    
    * reformat prettier

--- backend/src/loader.ts
+++ backend/src/loader.ts
@@ -17,42 +17,42 @@ export const sql = postgres({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
+})
 
 const setupRoutes = (app: Express) => {
-  const apiRouter = express.Router();
-  app.use("/api", apiRouter);
-  apiRouter.use("/testAPI", testAPIRoutes);
-  apiRouter.use("/user", userRoutes);
-  apiRouter.use("/issue", issueRoutes);
-};
+  const apiRouter = express.Router()
+  app.use("/api", apiRouter)
+  apiRouter.use("/testAPI", testAPIRoutes)
+  apiRouter.use("/user", userRoutes)
+  apiRouter.use("/issue", issueRoutes)
+}
 
 export const load = (app: Express) => {
   // Error handling middleware, we delegate the handling to the centralized error handler
-  app.use(logger("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+  app.use(logger("dev"))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(cookieParser())
   app.use(((e, req, res, _) => {
-    logError(e);
+    logError(e)
     if (e instanceof VError) {
-      res.status(VError.info(e).code ?? 500).send(e);
-    } else res.status(HttpCode.SERVER_ERROR).send(e);
-  }) as ErrorRequestHandler);
+      res.status(VError.info(e).code ?? 500).send(e)
+    } else res.status(HttpCode.SERVER_ERROR).send(e)
+  }) as ErrorRequestHandler)
 
-  setupRoutes(app);
-};
+  setupRoutes(app)
+}
 
 process.on("unhandledRejection", error => {
   // I just caught an unhandled promise rejection,
   // since we already have fallback handler for unhandled errors (see below),
   // let throw and let him handle that
-  console.log("unhandled Rejection");
-  throw error;
-});
+  console.log("unhandled Rejection")
+  throw error
+})
 
 process.on("uncaughtException", (error: Error) => {
   // I just received an error that was never handled, time to handle it and then decide whether a restart is needed
-  logError(error);
-  process.exit(1);
-});
+  logError(error)
+  process.exit(1)
+})
