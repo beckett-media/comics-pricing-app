184ddf4372279e2c14fa13fde430c05f02b83fe8
arsh <arsh_singh@live.com>
2022-05-04 02:37:05 -0500

    Fix prettier

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,59 +1,65 @@
 import axios from "axios"
-import React, { useState, ReactNode } from 'react';
-import useSWR from "swr";
+import React, { useState, ReactNode } from "react"
+import useSWR from "swr"
 
 type BlockProps = {
-  children?: ReactNode;
-};
+  children?: ReactNode
+}
 
 const SignUp = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
 
   const submit = async (event: React.SyntheticEvent) => {
     event.preventDefault()
     try {
       const emailEntry = {
         email: email,
-        name: name
-      };
-      const res = await axios.post("/api/user/signup", emailEntry);
+        name: name,
+      }
+      const res = await axios.post("/api/user/signup", emailEntry)
     } catch (e) {
-      console.log("Failed to add the new emails.");
+      console.log("Failed to add the new emails.")
     }
-  };
+  }
 
   return (
     <div className="rounded-2xl shadow-xl hover:shadow-2xl flex h-1/2 w-1/2 flex items-center justify-center">
-      <form
-        onSubmit={e => submit(e)}
-        className="px-8 pt-6 pb-8 mb-4"
-      >
+      <form onSubmit={(e) => submit(e)} className="px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
           <label>
-            <input type="email" placeholder={"email"}
-                   onChange={e => setEmail(e.target.value)}
-                   value={email}
-                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <input
+              type="email"
+              placeholder={"email"}
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </label>
         </div>
         <div className="mb-6">
-        <label>
-          <input placeholder={"name"} type="text"
-                 onChange={e => setName(e.target.value)}
-                 value={name}
-                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </label>
+          <label>
+            <input
+              placeholder={"name"}
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </label>
         </div>
         <div>
-          <button type={"submit"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Join!</button>
+          <button
+            type={"submit"}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Join!
+          </button>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
 const Block = (props: BlockProps) => (
   <div className="rounded-2xl shadow-xl hover:shadow-2xl flex h-1/2 w-1/2 flex items-center justify-center bg-slate-800 text-white font-bold">
