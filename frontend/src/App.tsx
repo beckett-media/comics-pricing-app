797246e210b106c5b958f86e59078080f8b13fb3
randomicons <srirambhat7@gmail.com>
2022-05-03 20:39:59 -0500

    swr setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,18 @@ import { Home } from "./pages/Home"
 import Dashboard from "./pages/Dashboard"
 import Search from "./pages/Search"
 import IssueDetails from "./pages/IssueDetails"
+import axios from "axios"
+import { SWRConfig } from "swr"
 
 export default function App() {
   return (
-    <div>
+    <SWRConfig value={{ fetcher: (url) => axios.get(url).then((res) => res.data) }}>
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/dashboard"} element={<Dashboard />} />
         <Route path={"/search/:query"} element={<Search />} />
         <Route path={"/details/:issueId"} element={<IssueDetails />} />
       </Routes>
-    </div>
+    </SWRConfig>
   )
 }
