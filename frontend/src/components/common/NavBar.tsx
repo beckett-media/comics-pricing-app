64148d03b81da82f8c93d8bc6d09f6538dae86c6
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-16 18:14:06 -0400

    navbar with searchbar template

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,7 +1,23 @@
+import SearchBar from "components/common/SearchBar"
+
 export default function NavBar() {
   return (
-    <div className="w-full h-20 bg-background flex flex-row justify-center items-center">
-      <div className="text-header">NavBar</div>
+    <div className="w-full h-24 bg-background grid grid-cols-navbar">
+      <div className="text-center text-header">Comics Surge</div>
+      <SearchBar />
+      <NavBarButtons />
+    </div>
+  )
+}
+
+function NavBarButtons() {
+  return (
+    <div className="flex flex-row p-2 space-x-2 items-center">
+      <div className="w-20 h-6 bg-button text-text text-center rounded-full">search</div>
+      <div className="grow flex flex-row justify-center space-x-2">
+        <div className="w-8 h-8 bg-button text-text text-center rounded-full">N</div>
+        <div className="w-8 h-8 bg-button text-text text-center rounded-full">M</div>
+      </div>
     </div>
   )
 }
---
+++ frontend/src/components/common/SearchBar.tsx
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="w-full h-8 rounded bg-field"></div>
+    </div>
+  )
+}
