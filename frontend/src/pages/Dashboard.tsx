4eca11ad341d4e74269d6209a897e56e61912e75
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-16 14:04:56 -0400

    clean up flexboxes in dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -12,24 +12,23 @@ import TrendingTitles from "../components/dashboard/TrendingTitles"
 export default function Dashboard() {
   return (
     <>
-      <div className="w-full md:px-10 space-y-10 mb-10">
-        <div className="flex flex-row space-x-10 pt-10 pl-10 pr-10">
+      <div className="w-full px-10 space-y-10 my-10">
+        <div className="flex flex-row space-x-10">
           <News />
           <div className="flex-col space-y-10 w-3/6">
             <RecentlyViewed />
           </div>
         </div>
-        <div className="flex flex-row space-x-10 pl-10 pr-10">
+        <div className="flex flex-row space-x-10">
           <TrendingTitles />
           <RecentPriceDrops />
           <NewComics />
         </div>
-        <div className="w-full md:px-10 space-y-10 mb-10">
-          <PopularPublishers />
-          <PopularComics />
-          <PopularSeries />
-          <BrowseByEra />
-        </div>
+
+        <PopularPublishers />
+        <PopularComics />
+        <PopularSeries />
+        <BrowseByEra />
       </div>
       <Footer />
     </>
