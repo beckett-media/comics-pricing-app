7e4179d902df9fcbc4c3e08204d7b6fbbf5d42f4
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-16 17:47:28 -0400

    initial colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      'background': '#161032',
+      'field': '#433b64',
+      'header': '#fffffe',
+      'text': '#bcc5d2',
+      'link': '#41d3f3',
+      'button': '#5416e5',
+    },
     extend: {},
   },
   plugins: [],
