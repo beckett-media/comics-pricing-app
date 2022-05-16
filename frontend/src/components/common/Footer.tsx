191416891cd4d3f55bf30fccdf5e36f483cb76e0
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-16 17:47:45 -0400

    logo & footer/navbar basic styles

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,7 +1,12 @@
+import Logo from "components/common/Logo"
+
 export default function Footer() {
   return (
-    <div className="w-full h-40 bg-slate-300 flex flex-row justify-center items-center">
-      <div>Footer</div>
+    <div className="w-full h-40 bg-background flex flex-col justify-between items-start p-10">
+      <div className="h-8 w-32">
+        <Logo />
+      </div>
+      <div className="text-xs text-header">Visit Site</div>
     </div>
   )
 }
--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
   return (
-    <div className="w-full h-20 bg-slate-300 flex flex-row justify-center items-center">
-      <div>NavBar</div>
+    <div className="w-full h-20 bg-background flex flex-row justify-center items-center">
+      <div className="text-header">NavBar</div>
     </div>
   )
 }
