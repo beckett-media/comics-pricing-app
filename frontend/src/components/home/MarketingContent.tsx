4333f8e711f91027f1a166f45e1c29834fd94aad
arsh <arsh_singh@live.com>
2022-05-16 22:57:05 -0500

    Address pr comments

--- frontend/src/components/home/MarketingContent.tsx
+++ frontend/src/components/home/MarketingContent.tsx
@@ -1,66 +1,9 @@
-import axios from "axios"
-import React, { useState } from "react"
-import toast, { Toaster } from "react-hot-toast"
+import { useState } from "react"
+import { Toaster } from "react-hot-toast"
+import SignUp from "components/home/SignUp"
 
 export default function MarketingContent() {
   const [showForm, setShowForm] = useState(true)
-
-  const setShowFormToTrue = () => setShowForm(false)
-  const notify = (message: string) => toast(message)
-
-  const SignUp = () => {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-
-    const submit = async (event: React.SyntheticEvent) => {
-      event.preventDefault()
-      try {
-        const emailEntry = { email, name }
-        await axios.post("/api/user/waitlist", emailEntry)
-        setShowFormToTrue()
-        notify("Successfully added to the waitlist!")
-      } catch (e) {
-        notify("Error: Failed to add you to the waitlist!")
-      }
-    }
-
-    return (
-      <form onSubmit={(e) => submit(e)} className="flex flex-col space-y-3">
-        <label>
-          <input
-            placeholder={"your name"}
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </label>
-        <label>
-          <input
-            type="email"
-            placeholder={"your best email"}
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            className="shadow appearance-none border  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </label>
-        <button
-          type={"submit"}
-          className="bg-white hover:bg-slate-100 text-black py-3 w-full focus:outline-none focus:shadow-outline"
-        >
-          Join the waitlist
-        </button>
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            style: {
-              background: "#FFFFFF",
-            },
-          }}
-        />
-      </form>
-    )
-  }
 
   return (
     <div className="flex flex-col items-center space-y-7 bg-black h-full w-full pt-7 pb-7">
@@ -71,7 +14,15 @@ export default function MarketingContent() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua.
       </p>
-      {showForm ? <SignUp /> : null}
+      {showForm ? <SignUp setShowForm={setShowForm} /> : null}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: "#FFFFFF",
+          },
+        }}
+      />
       <img
         className="object-scale-down h-96 "
         src="https://nickpassey.com/wp-content/uploads/2016/10/Featured.jpg"
