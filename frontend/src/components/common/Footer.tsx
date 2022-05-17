4033b64514014be456802e9e2d577f3cdc2cebb1
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-17 08:29:53 -0400

    basic layout for navbar and footer

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <div className="w-full h-40 bg-slate-300 flex flex-row justify-center items-center">
-      <div>Footer</div>
+    <div className="w-full h-40 bg-slate-300 flex flex-row justify-center items-center space-x-10">
+      <div>Link 1</div>
+      <div>Beckett Logo</div>
+      <div>Link 2</div>
     </div>
   )
 }
--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,7 +1,37 @@
 export default function NavBar() {
   return (
-    <div className="w-full h-20 bg-slate-300 flex flex-row justify-center items-center">
-      <div>NavBar</div>
+    <div className="w-full h-24 bg-slate-300 grid grid-cols-navbar">
+      <div className="flex flex-col justify-center items-center text-center text-2xl">
+        <div>COMIC SURGE</div>
+      </div>
+      <SearchBar />
+      <NavBarButtons />
+    </div>
+  )
+}
+
+function SearchBar() {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <input className="w-full h-8 px-2 bg-slate-100 rounded" placeholder="Search"></input>
+    </div>
+  )
+}
+
+function NavBarButtons() {
+  return (
+    <div className="flex flex-row p-2 space-x-2 items-center">
+      <button className="w-20 h-8 bg-slate-100 hover:bg-slate-200 text-text text-center rounded-full">
+        search
+      </button>
+      <div className="grow flex flex-row justify-center space-x-2">
+        <button className="w-8 h-8 bg-slate-100 hover:bg-slate-200 text-center rounded-full">
+          N
+        </button>
+        <button className="w-8 h-8 bg-slate-100 hover:bg-slate-200 text-center rounded-full">
+          M
+        </button>
+      </div>
     </div>
   )
 }
