fd598c32eef6e669fa1ec4708f6da9779da54bba
arshy <30162549+arshtsingh@users.noreply.github.com>
2022-05-17 22:39:19 -0500

    Ats/landingpage (#39)

---
+++ frontend/src/components/home/MarketingContent.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { Toaster } from "react-hot-toast"
+import SignUp from "components/home/SignUp"
+
+export default function MarketingContent() {
+  const [showForm, setShowForm] = useState(true)
+
+  return (
+    <div className="flex flex-col items-center space-y-7 bg-black h-screen w-full pt-7 pb-7">
+      <p className="text-white text-2xl font-bold font-roboto text-center">
+        The best comic prices at your fingertips
+      </p>
+      <p className="text-gray-400 font-roboto font-thin w-64 h-55 text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </p>
+      {showForm ? <SignUp setShowForm={setShowForm} /> : null}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: "#FFFFFF",
+          },
+        }}
+      />
+      {/* <img
+        className="object-scale-down h-96"
+        src="https://nickpassey.com/wp-content/uploads/2016/10/Featured.jpg"
+        alt=""
+      ></img> */}
+    </div>
+  )
+}
---
+++ frontend/src/components/home/SignUp.tsx
@@ -0,0 +1,62 @@
+import axios from "axios"
+import toast, { Toaster } from "react-hot-toast"
+import { useState } from "react"
+
+const SignUp: React.FC<{ setShowForm: React.Dispatch<React.SetStateAction<boolean>> }> = ({
+  setShowForm,
+}) => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  const submit = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    try {
+      const emailEntry = { email, name }
+      await axios.post("/api/user/waitlist", emailEntry)
+      setShowForm(false)
+      toast("Successfully added to the waitlist!")
+    } catch (e) {
+      toast("Error: Failed to add you to the waitlist!")
+      console.log(e)
+    }
+  }
+
+  return (
+    <form onSubmit={(e) => submit(e)} className="flex flex-col space-y-3">
+      <label>
+        <input
+          placeholder={"your name"}
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </label>
+      <label>
+        <input
+          type="email"
+          placeholder={"your best email"}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </label>
+      <button
+        type={"submit"}
+        className="bg-white hover:bg-slate-100 text-black py-3 w-full focus:outline-none focus:shadow-outline"
+      >
+        Join the waitlist
+      </button>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: "#FFFFFF",
+          },
+        }}
+      />
+    </form>
+  )
+}
+
+export default SignUp
