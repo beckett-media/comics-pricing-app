2fc9c3b940381221579fad5f20e61387d396bac9
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-18 17:44:53 -0400

    useSWR + formatting

--- frontend/src/components/dashboard/News.tsx
+++ frontend/src/components/dashboard/News.tsx
@@ -1,66 +1,32 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
-// import useSWR from "swr"
-
-const newsApiURL =
-  "https://newsapi.org/v2/everything?q=Comics&from=2022-05-17&sortBy=relevancy&pageSize=1&apiKey=562595bc762f4eb8a9aed8763c3b08be"
+import useSWR from "swr"
 
 export default function News() {
-  const [newsApi, setNewsApi] = useState<any>(null)
+  const { data: news } = useNews()
 
-  useEffect(() => {
-    axios.get(newsApiURL).then((response: any) => {
-      setNewsApi(response.data.articles[0])
-    })
-  }, [])
+  if (!news) {
+    return <div>loading</div>
+  }
+
+  const { url, urlToImage, title } = news.articles[0]
 
   return (
     <div className="w-7/12">
-      {newsApi ? (
-        <a href={newsApi.url} className="relative flex h-full flex-col ">
-          <img
-            src={newsApi.urlToImage}
-            className="absolute h-full w-full overflow-hidden rounded object-cover"
-            alt=""
-          />
-          <span className="z-0 m-5 text-lg text-white">
-            Headline Comic Article:
-            <br />
-            {newsApi.title}
-          </span>
-        </a>
-      ) : (
+      <a href={url} className="relative flex h-full flex-col ">
         <img
-          src="https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"
-          alt=""
+          src={urlToImage}
+          className="h-full w-full overflow-hidden rounded object-cover"
+          alt={title}
         />
-      )}
+        <span className="absolute z-0 m-5 text-lg text-white">{title}</span>
+      </a>
     </div>
   )
 }
 
-// export default function News() {
-//   const { data: newsApi, error } = useSWR<any>(newsApiURL, fetcher)
-//   console.log(error)
-//   if (error) {
-//     return <div>{error.toString()}</div>
-//   }
+function useNews() {
+  const url =
+    "https://newsapi.org/v2/everything?q=Comics&from=2022-05-17&sortBy=relevancy&pageSize=1&apiKey=562595bc762f4eb8a9aed8763c3b08be"
 
-//   if (!newsApi) {
-//     return <div>Loading...</div>
-//   }
-
-//   return (
-//     <a href={newsApi.url} className="relative flex h-full flex-col ">
-//       <img
-//         src={newsApi.urlToImage}
-//         className="absolute h-full w-full overflow-hidden rounded object-cover"
-//       />
-//       <span className="z-0 m-5 text-lg text-white">
-//         Headline Comic Article:
-//         <br />
-//         {newsApi.title}
-//       </span>
-//     </a>
-//   )
-// }
+  return useSWR<any>(url, (url) => axios.get(url).then((res) => res.data))
+}
