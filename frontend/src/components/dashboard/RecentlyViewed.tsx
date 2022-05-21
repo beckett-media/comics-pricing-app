61e74429538cca42f6522c6c07fd797529068be0
michael <66692526+mkxl@users.noreply.github.com>
2022-05-21 00:18:26 -0400

    update search and navbar (#50)
    * update search and navbar
    
    * remove any

--- frontend/src/components/dashboard/RecentlyViewed.tsx
+++ frontend/src/components/dashboard/RecentlyViewed.tsx
@@ -5,8 +5,8 @@ export default function RecentlyViewed() {
   return (
     <div className="w-5/12 overflow-hidden rounded border-2 p-2">
       <Gallery title="Recently Viewed">
-        {[...Array(5).keys()].map((_) => (
-          <Issue />
+        {[...Array(5).keys()].map(idx => (
+          <Issue key={idx.toString()} />
         ))}
       </Gallery>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "nav-bar": "#0C0822",
+        "search-bar": "#343141",
+        "search-text": "#0C0822",
+        "search-button-start": "#C3FAE4",
+        "search-button-stop": "#6CD7D4",
+      },
       gridTemplateColumns: {
         navbar: "1fr 40% 1fr",
       },
