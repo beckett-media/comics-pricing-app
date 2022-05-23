4ae8fd14cf69267d8e99f8e69173265f45490566
michael <66692526+mkxl@users.noreply.github.com>
2022-05-23 02:17:34 -0400

    update ingress paths

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -13,9 +13,6 @@ export const userRoutes = Router()
 
 userRoutes.get("/login", async (req: RequestWithQueryParams<{ code: string, state: string }>, res: Response) => {
   try {
-    console.log(">>> REDIRECT_URI", `${req.protocol}://${req.headers.host}/api/user/login/`)
-    console.log(">>> HEADERS", req.headers)
-
     const cognitoRes = await axios.post(
       COGNITO_API_URL,
       querystring.stringify({
