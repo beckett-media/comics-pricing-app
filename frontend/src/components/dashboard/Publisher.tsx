b485b436d318fcf67071b7d435cc7a96ba4473f3
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-24 23:28:58 -1000

    consistent bolding of issues, titles, publishers

--- frontend/src/components/dashboard/Publisher.tsx
+++ frontend/src/components/dashboard/Publisher.tsx
@@ -9,11 +9,11 @@ function image(id: string) {
 
 export default function Publisher({ id, name }: PublisherProps) {
   return (
-    <div className="flex h-40 w-40 flex-col items-center">
+    <div className="flex h-40 w-40 flex-col items-center gap-3">
       <div className="h-32 w-40">
         <img className="h-32 w-40 object-contain" alt={name} src={image(id)} />
       </div>
-      <p className="text-center text-xs">{name}</p>
+      <p className="text-center text-xs font-semibold whitespace-nowrap">{name} Comics</p>
     </div>
   )
 }
