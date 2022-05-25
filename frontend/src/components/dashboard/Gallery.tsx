9fa7b3d45a109e9cf1c86aedff62c2ee9f0a6afe
michael <66692526+mkxl@users.noreply.github.com>
2022-05-25 13:19:01 -0400

    add gradient button component

--- frontend/src/components/dashboard/Gallery.tsx
+++ frontend/src/components/dashboard/Gallery.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react"
 
+import GradientButton from "components/common/GradientButton"
+
 type GalleryProps = {
   title: string
   children?: ReactNode
@@ -8,9 +10,9 @@ type GalleryProps = {
 export default function Gallery({ title, children }: GalleryProps) {
   return (
     <div className="flex w-full flex-col rounded bg-container-outer p-7 text-common-text">
-      <div>
+      <div className="flex justify-between">
         <span className="mr-5 font-semibold">{title}</span>
-        <span className="cursor-pointer text-xs hover:underline">View All ›</span>
+        <GradientButton text="View All" bg="bg-container-outer" />
       </div>
       <div className="flex w-full flex-row justify-between gap-10 overflow-x-auto pt-5">
         {children}
