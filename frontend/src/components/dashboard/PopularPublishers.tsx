7b42b6af3b8b8a62a7beefb2551127a38ab436ad
Christian Kaman <christiank@tekimax.com>
2022-06-08 13:01:07 -0400

    Feature/api endpoint (#91)
    * update app.tsx
    
    * update api

--- frontend/src/components/dashboard/PopularPublishers.tsx
+++ frontend/src/components/dashboard/PopularPublishers.tsx
@@ -1,11 +1,33 @@
 import useSWR from "swr"
+import * as React from "react"
 
 import Gallery from "./Gallery"
 import Publisher from "./Publisher"
 import type { Publisher as PublisherData } from "types/api"
+import { API } from "aws-amplify"
 
 export default function PopularPublishers() {
-  const { data: publishers } = useSWR<PublisherData[]>("/api/publisher/popular")
+  // const { data: publishers } = useSWR<PublisherData[]>("/api/publisher/popular")
+
+  const [publishers, setData] = React.useState<PublisherData[]>([])
+
+  const apiName = "comicsapi"
+  const path = "/api/publisher/popular"
+  const myInit = {
+    // OPTIONAL
+    response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+  }
+
+  React.useEffect(() => {
+    API.get(apiName, path, myInit)
+      .then((response) => {
+        // Add your code here
+        setData(response?.data?.rows)
+      })
+      .catch((error) => {
+        console.log(error.response)
+      })
+  }, [])
 
   if (!publishers) {
     return <div>loading</div>
--- frontend/src/components/issue-details/RelatedTitles.tsx
+++ frontend/src/components/issue-details/RelatedTitles.tsx
@@ -2,9 +2,33 @@ import useSWR from "swr"
 import React from "react"
 import { getTitleImage } from "../../utils/imagePath"
 import { Title } from "types/api"
+import { API } from "aws-amplify"
 
 const RelatedIssues: React.FC<{ issueId: string }> = ({ issueId }) => {
-  const { data: relatedTitles } = useSWR<Title[]>(`/api/issue/${issueId}/related/titles`)
+  // const { data: relatedTitles } = useSWR<Title[]>(`/api/issue/${issueId}/related/titles`)
+
+  const [relatedTitles, setData] = React.useState<Title[]>()
+  const [error, setError] = React.useState<any>()
+
+  const apiName = "comicsapi"
+  const path = `/api/issue/%27${issueId}%27/related/titles`
+  const myInit = {
+    // OPTIONAL
+    response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+  }
+
+  React.useEffect(() => {
+    API.get(apiName, path, myInit)
+      .then((response) => {
+        // Add your code here
+        console.log("relates title",response?.data)
+      })
+      .catch((error) => {
+        console.log(error.response)
+        setError(error)
+      })
+  }, [issueId])
+
   return (
     <div>
       <p className={"text-center"}>
