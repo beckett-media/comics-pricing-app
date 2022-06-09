cc84e8efc64527db5876c9d055af44eca08fe735
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-06-09 17:58:22 +0200

    created scss setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "@fortawesome/fontawesome-free/css/all.css"
 import "./index.css"
+import "./styles/main.scss"
 import { ChakraProvider } from "@chakra-ui/react"
 import Amplify, { Auth } from 'aws-amplify';
 
