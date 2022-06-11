4f4e1aca23f7ad9a3258ed432ad39b8ea608153e
Nasser Qadri <nasserqadri@gmail.com>
2022-06-11 00:36:20 -0400

    rebuild

--- frontend/src/components/dashboard/RecentlyViewed.tsx
+++ frontend/src/components/dashboard/RecentlyViewed.tsx
@@ -4,7 +4,7 @@ import Issue from "./Issue"
 export default function RecentlyViewed() {
   return (
     <div className="overflow-hidden rounded">
-      <Gallery title="Recently Viewed">
+      <Gallery title="Recently Viewed" fullScreen={false}>
         {/* <Issue />
         <Issue />
         <Issue />
