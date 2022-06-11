8c3e527e7ded54974392d9e48d5580a47f747627
Nasser Qadri <102703529+nasser-psd@users.noreply.github.com>
2022-06-11 01:11:53 -0400

    browse by era (#113)
    Co-authored-by: Nasser Qadri <nasserqadri@gmail.com>

--- frontend/src/components/dashboard/BrowseByEra.tsx
+++ frontend/src/components/dashboard/BrowseByEra.tsx
@@ -1,3 +1,35 @@
+import {
+  Image,
+} from "@chakra-ui/react"
+
+import {Link} from "react-router-dom"
+
+
 export default function BrowseByEra() {
-  return <h1>Browse By Era</h1>
+  return  (
+    <div className="w-full overflow-hidden rounded">
+
+      
+      <h1 className='heading mr-5 text-xl font-semibold'>Browse By Era</h1>
+
+      <div className="flex w-full flex-row  gap-10  pt-3  ">
+
+      <Link to={`/search`} className="">
+        <Image src={require("assets/GoldenAge.png")} alt="logo" />
+      </Link>
+      <Link to={`/search`} className="">
+        <Image src={require("assets/SilverAge.png")} alt="logo" />
+      </Link>
+      <Link to={`/search`} className="">
+        <Image src={require("assets/BronzeAge.png")} alt="logo" />
+      </Link>
+      <Link to={`/search`} className="">
+        <Image src={require("assets/ModernAge.png")} alt="logo" />
+      </Link> 
+      </div>
+    </div>
+
+  )
+
+
 }
