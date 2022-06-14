d9dfdfad27f1d47ee13900a9576c788538c46c0c
Nasser Qadri <102703529+nasser-psd@users.noreply.github.com>
2022-06-14 14:12:36 -0400

    fixing issues page (#151)
    Co-authored-by: Nasser Qadri <nasserqadri@gmail.com>

--- frontend/src/hooks/data/usePriceAnalytics.ts
+++ frontend/src/hooks/data/usePriceAnalytics.ts
@@ -2,7 +2,7 @@ import useSWR, { useSWRConfig } from "swr";
 import { API } from "aws-amplify";
 
 export default function usePriceAnalytics(id:string, num_months:string) {
-  const { data, error } = useSWR<any>(`/issue/'${id}'/${num_months}/issue-price-analytics`);
+  const { data, error } = useSWR<any>(`/issue/${id}/${num_months}/issue-price-analytics`);
   
 
   return {
