230eca8af44f97eaeaffccf61d31c6dbf75d59cf
Raj Shah <rajshah@Rajs-MBP.mynetworksettings.com>
2022-06-14 14:23:28 -0400

    NP: Fixed order of Eras

--- frontend/src/components/search/Refinement.tsx
+++ frontend/src/components/search/Refinement.tsx
@@ -13,7 +13,7 @@ type RefinementProps = {
 
 const REFINEMENT_LIST_LIMIT = 16
 
-export default function Refinement({ attribute, title }: RefinementProps) {
+export function Refinement({ attribute, title }: RefinementProps) {
     return (
       <div className="flex flex-col">
         <p className="text-lg">{title}</p>
@@ -29,5 +29,34 @@ export default function Refinement({ attribute, title }: RefinementProps) {
         />
       </div>
     )
+}
+
+export function RefinementEra( { attribute, title }: RefinementProps ) {
+  console.log("RefinementEra");
+    return (
+      <div className="flex flex-col">
+      <p className="text-lg">{title}</p>
+      <RefinementList
+        attribute={attribute}
+        limit={REFINEMENT_LIST_LIMIT}
+        sortBy={(a,b) => sortEra(a.name, b.name)}
+        classNames={{
+          label: "flex gap-2 items-center",
+          labelText: "",
+          count: "text-slate-500",
+        }}
+      />
+      </div>
+    )
+}
+
+function sortEra(A: string, B: string) {
+  const eraDict = {
+    "Modern": 1,
+    "Copper": 2,
+    "Bronze": 3,
+    "Silver": 4,
+    "Gold": 5
   }
-  
+  return eraDict[A as keyof typeof eraDict] - eraDict[B as keyof typeof eraDict];
+}
