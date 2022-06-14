523d4a1348ed72ab0ae38b5fa0bc01d44ce6cec1
Nasser Qadri <nasserqadri@gmail.com>
2022-06-14 14:35:17 -0400

    Gallery padding hotfix

--- frontend/src/components/dashboard/Gallery.tsx
+++ frontend/src/components/dashboard/Gallery.tsx
@@ -10,12 +10,12 @@ type GalleryProps = {
 
 function Gallery({ title, children, link, fullScreen }: GalleryProps) {
   return (
-    <div className="flex w-full flex-col rounded bg-container-outer py-5 px-3 text-common-text">
+    <div className="flex w-full flex-col rounded bg-container-outer md:py-2 px-3 text-common-text">
       {fullScreen && (<div className="flex justify-between items-center">
         <span className="heading mr-5 text-xl font-semibold">{title}</span>
         <Link to={link} className="button button-secondary">View All ›</Link>
       </div>)}
-      {!fullScreen && (<div className="flex justify-center items-center py-2">
+      {!fullScreen && (<div className="flex justify-center items-center">
       <Link to={link} className="button button-secondary"><span className="heading font-semibold">{title}</span></Link>
       </div>)}
       <div className="flex w-full flex-row  gap-10 overflow-x-auto pt-3">
